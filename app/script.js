36cc47d2e56da33e137874b1dd6c8487e753c1a1
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-01-19 14:30:16 +0530

    Adding a movie via post

--- app/script.js
+++ app/script.js
@@ -66,5 +66,77 @@ function getMoviesFromAPI() {
     });
 }
 
+function createMovieInAPI(movie) {
+  fetch("http://localhost:1337/api/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movie),
+  })
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (data) {
+      console.log("API success");
+      console.log(data);
+    });
+}
+
+function hookButtons() {
+  // add-movies-btn
+  const addBtn = document.querySelector("#add-movies-btn");
+  addBtn.addEventListener("click", function () {
+    const el1 = document.querySelector("#all-movies");
+    el1.classList.add("d-none");
+    const el2 = document.querySelector("#add-movies");
+    el2.classList.remove("d-none");
+  });
+
+  // show-movies-btn
+  const showMoviesBtn = document.querySelector("#show-movies-btn");
+  showMoviesBtn.addEventListener("click", function () {
+    const el1 = document.querySelector("#all-movies");
+    el1.classList.remove("d-none");
+    const el2 = document.querySelector("#add-movies");
+    el2.classList.add("d-none");
+  });
+}
+
+function hookAddMovieForm() {
+  const form = document.querySelector("#add-movie-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    /*
+    {
+    "name": "The Green Lantern",
+    "year": "2007",
+    "rating": "1",
+    "genre": "Super hero",
+    "image": {
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzyF4A_SMjtSjIo2_BxxWaicBQvUI9jaqwxIu1-qDS-RnSBfS2IRBQtuvN_3u2-y-RKR0&usqp=CAU",
+        "altInfo": "Green lantern DVD cover"
+      }
+    }
+    */
+    const movie = {
+      name: formData.get("movie-name"),
+      year: formData.get("movie-year"),
+      rating: formData.get("movie-rating"),
+      genre: formData.get("movie-genre"),
+      image: {
+        url: formData.get("movie-image-url"),
+        altInfo: formData.get("movie-image-alt"),
+      },
+    };
+
+    createMovieInAPI(movie);
+  });
+}
+
 // --- start
 getMoviesFromAPI();
+hookButtons();
+hookAddMovieForm();
